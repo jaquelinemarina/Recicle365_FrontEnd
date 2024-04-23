15cbf9acbf031c00b6e766298ef8d33019c8cd97
Jaqueline Marina <jaquelinemarina@hotmail.com>
2024-04-23 18:12:10 -0300

    feat (header): criando header da aplicação

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css'
+import Header from './components/header/Header.jsx'
 
 function App() {
 
   return (
     <>
+      <Header />
     </>
   )
 }
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,14 @@
+import style from "./header.module.css"
+import { Link } from "react-router-dom"
+
+function Header() {
+    return (
+        <header className={style.container}>
+            <Link to="/" className={style.dashboard}>Recicle365</Link>
+            <Link to="/listaLCR" className={style.listaLCR}>Locais de Coleta</Link>
+            <Link to="/cadastroLCR" className={style.cadastroLCR}>Cadastrar Local de Coleta</Link>
+        </header>
+    )
+}
+
+export default Header
