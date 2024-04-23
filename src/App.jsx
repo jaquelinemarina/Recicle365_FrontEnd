a1c08020fc496bc2489bdcfb69cc0b3d16eda244
Jaqueline Marina <jaquelinemarina@hotmail.com>
2024-04-23 18:44:10 -0300

    feat (App.jsx): add Outlet

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
 import Header from './components/header/Header.jsx'
 import Footer from './components/footer/Footer.jsx'
+import { Outlet } from 'react-router-dom'
 
 function App() {
 
   return (
     <>
       <Header />
+      <Outlet />
       <Footer />
     </>
   )
