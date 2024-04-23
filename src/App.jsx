3b58e551d3a1e87f449c42eb4d28899b03a19bd3
Jaqueline Marina <jaquelinemarina@hotmail.com>
2024-04-23 18:42:15 -0300

    feat (App.jsx): importando footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import Header from './components/header/Header.jsx'
+import Footer from './components/footer/Footer.jsx'
 
 function App() {
 
   return (
     <>
       <Header />
+      <Footer />
     </>
   )
 }
