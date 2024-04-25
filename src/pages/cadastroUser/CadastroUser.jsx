a724f57ac7797224801611eaffb094626015b84b
Jaqueline Marina <jaquelinemarina@hotmail.com>
2024-04-25 08:32:36 -0300

    fix (CadastroUser):  add inputs que faltavam

--- src/pages/cadastroUser/CadastroUser.jsx
+++ src/pages/cadastroUser/CadastroUser.jsx
@@ -12,7 +12,7 @@ function CadastroUser() {
     function onSubmit(formValue) {
         console.log("Formulário enviado", formValue)
 
-        //addUser({ ...formValue, cpf: Number(formValue.cpf) })
+        //addUser({ ...formValue, cpf: Number(formValue.cpf), number: Number(formValue.number), cep: Number(formValue.cep)})
     }
 
     return (
@@ -25,7 +25,7 @@ function CadastroUser() {
                     <input placeholder="digite o seu nome completo"
                         type="text"
                         {...register("name", {
-                            required: "Por favor, digite seu nome completo.",
+                            required: "Campo obrigatório.",
                             minLength: { value: 3, message: "Insira um nome válido" },
                             maxLength: { value: 60, message: "Máximo 60 caracteres" },
                         })}
@@ -33,12 +33,12 @@ function CadastroUser() {
                     {errors?.name && <p>{errors.name.message}</p>}
                 </div>
 
-                <div className={style.cpf}> {/* pegar dados do 4Devs */}
+                <div className={style.cpf}> 
                     <label htmlFor="cpf">CPF</label>
                     <input placeholder="digite o seu CPF"
                         type="number"
                         {...register("cpf", {
-                            required: "Por favor, digite seu CPF.",
+                            required: "Campo obrigatório.",
                             minLength: { value: 11, message: "Insira um CPF válido" },
                             maxLength: { value: 11, message: "Máximo 11 caracteres" },
                         })}
@@ -50,7 +50,7 @@ function CadastroUser() {
                     <label htmlFor="sex">Sexo</label>
                     <select name="sex" id="sex"
                         {...register("sex", {
-                            required: "Por favor, selecione uma opção."
+                            required: "Campo obrigatório."
                         })}>
                         <option value="">selecione uma opção</option>
                         <option value="Homem">Homem</option>
@@ -65,7 +65,7 @@ function CadastroUser() {
                     <input placeholder="digite a data do seu nascimento"
                         type="datetime"
                         {...register("born", {
-                            required: "Por favor, digite a data do seu nascimento.",
+                            required: "Campo obrigatório.",
                             minLength: { value: 8, message: "Insira uma data válida" },
                             maxLength: { value: 8, message: "Máximo 8 caracteres" },
                         })}
@@ -78,7 +78,7 @@ function CadastroUser() {
                     <input placeholder="digite seu email"
                         type="email"
                         {...register("email", {
-                            required: "Por favor, digite seu email.",
+                            required: "Campo obrigatório.",
                             minLength: { value: 3, message: "Insira uma email válido" },
                             maxLength: { value: 60, message: "Máximo 60 caracteres" },
                         })}
@@ -86,17 +86,29 @@ function CadastroUser() {
                     {errors?.email && <p>{errors.email.message}</p>}
                 </div>
 
-
                 <div className={style.password}>
                     <label htmlFor="password">Senha</label>
                     <input placeholder="digite sua senha"
                         type="password"
                         {...register("password", {
-                            required: "Por favor, digite sua senha.",
+                            required: "Campo obrigatório.",
                             minLength: { value: 6, message: "Mínimo 6 caracteres" },
                         })}
                     />
                     {errors?.password && <p>{errors.password.message}</p>}
+                </div>
+
+                <div className={style.cep}>
+                    <label htmlFor="cep">CEP</label>
+                    <input placeholder="digite o CEP"
+                        type="number"
+                        {...register("cep", {
+                            required: "Campo obrigatório.",
+                            minLength: { value: 3, message: "Insira um número válido" },
+                            maxLength: { value: 9, message: "Máximo 9 caracteres" },
+                        })}
+                    />
+                    {errors?.cep && <p>{errors.cep.message}</p>}
                 </div>
 
                 <div className={style.address}> {/* usar dados da API ViaCEP - provavelmente para preenchimento automático */}
@@ -104,12 +116,88 @@ function CadastroUser() {
                     <input placeholder="digite o seu endereço"
                         type="text"
                         {...register("address", {
-                            required: "Por favor, digite seu endereço.",
+                            required: "Campo obrigatório.",
                             minLength: { value: 3, message: "Insira um endereço válido" },
                             maxLength: { value: 60, message: "Máximo 60 caracteres" },
                         })}
                     />
                     {errors?.address && <p>{errors.address.message}</p>}
+                </div>
+
+                <div className={style.number}>
+                    <label htmlFor="number">Número</label>
+                    <input placeholder="digite o número da sua residência"
+                        type="number"
+                        {...register("number", {
+                            required: "Campo obrigatório.",
+                            minLength: { value: 1, message: "Insira um número válido" },
+                            maxLength: { value: 5, message: "Máximo 5 caracteres" },
+                        })}
+                    />
+                    {errors?.number && <p>{errors.number.message}</p>}
+                </div>
+
+                <div className={style.neighborhood}>
+                    <label htmlFor="neighborhood">Bairro</label>
+                    <input placeholder="digite o seu bairro"
+                        type="text"
+                        {...register("neighborhood", {
+                            required: "Campo obrigatório.",
+                            minLength: { value: 3, message: "Insira um nome válido" },
+                            maxLength: { value: 60, message: "Máximo 60 caracteres" },
+                        })}
+                    />
+                    {errors?.neighborhood && <p>{errors.neighborhood.message}</p>}
+                </div>
+
+                <div className={style.city}>
+                    <label htmlFor="city">Cidade</label>
+                    <input placeholder="digite a cidade"
+                        type="text"
+                        {...register("city", {
+                            required: "Campo obrigatório.",
+                            minLength: { value: 3, message: "Insira um nome válido." },
+                            maxLength: { value: 60, message: "Máximo 60 caracteres." },
+                        })} />
+                    {errors?.city && <p>{errors.city.message}</p>}
+                </div>
+
+                <div className={style.state}>
+                    <label htmlFor="state">Estado</label>
+                    <select name="state" id="state"
+                        {...register("state", {
+                            required: "Por favor, selecione uma opção."
+                        })}>
+                        <option value="">selecione o estado</option>
+                        <option value="AC">AC</option>
+                        <option value="AL">AL</option>
+                        <option value="AP">AP</option>
+                        <option value="AM">AM</option>
+                        <option value="BA">BA</option>
+                        <option value="CE">CE</option>
+                        <option value="DF">DF</option>
+                        <option value="ES">ES</option>
+                        <option value="GO">GO</option>
+                        <option value="MA">MA</option>
+                        <option value="MT">MT</option>
+                        <option value="MS">MS</option>
+                        <option value="MG">MG</option>
+                        <option value="PA">PA</option>
+                        <option value="PB">PB</option>
+                        <option value="PR">PR</option>
+                        <option value="PE">PE</option>
+                        <option value="PI">PI</option>
+                        <option value="RJ">RJ</option>
+                        <option value="RN">RN</option>
+                        <option value="RS">RS</option>
+                        <option value="RO">RO</option>
+                        <option value="RR">RR</option>
+                        <option value="SC">SC</option>
+                        <option value="SP">SP</option>
+                        <option value="SE">SE</option>
+                        <option value="TO">TO</option>
+                    </select>
+                    {errors?.state && <p>{errors.state.message}</p>}
                 </div>
 
                 <button className={style.btnRegister} type="submit">Cadastrar </button>
