f371e2f872233e17243ef247e5f010516b943e64
Jaqueline Marina <jaquelinemarina@hotmail.com>
2024-04-26 16:37:45 -0300

    fix (login): corrigindo função deleteUser que não estava funcionando

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,66 +1,63 @@
-import { useContext } from 'react'
+import { useContext, useEffect } from 'react'
 import { UsersContext } from '../../context/usersContext'
 import style from './login.module.css'
 
+//--------------------------------------------------------------------------
+// O QUE FALTA:
+// - criar o form de login de forma correta
+// - criar a validação de login no botão
+// - criar a navegação para a página de dashboard, caso validado
+// - criar botão/lógica para fazer login com um dos usuários já cadastrados
+// -------------------------------------------------------------------------
+
 function Login() {
 
-    const { users, registerUser, removeUser } = useContext(UsersContext)
+    const { users, deleteUser } = useContext(UsersContext)
 
-    useContext(() => {
+    useEffect(() => {
         console.log(users)
-    }, [])
+    }, [users])
 
     return (
-        <>
-            {/* deveria estar aqui? */}
-            {!!users && users.map((user) => (
-                <>
-                    <h3 key={user.id}>{user.name}</h3>
-                    <button onClick={() => removeUser(user.id)}>Remover</button>
-                </>
-            ))}
+        <div className={style.container}>
+            <div className={style.users}>
 
-            <div className={style.container}>
-                <div className={style.users}>
-                    <h4 className={style.title}>Usuários Cadastrados</h4>
-                    {/* O sistema quando iniciado irá carregar do localStorage ou json-server uma lista de 
-                usuários já cadastrados que usam a plataforma (criar uma lista com no mínimo 5 usuários, use o 4Devs) */}
-                    <li className={style.list}>
-                        <ul>Jaqueline Marina</ul>
-                        <ul>Pedro Salini</ul>
-                        <ul>Fernanda Silva</ul>
-                        <ul>Lucio Santos</ul>
-                        <ul>Ana Rocha</ul>
-                    </li>
-                </div>
+                {/* exibe a lista de usuários cadastrados no json */}
+                <h2 className={style.title}>Usuários Cadastrados</h2>
+                {!!users && users.map((user) => (
+                    <div key={user.id}>
+                        <h2>{user.name}</h2>
+                        <button onClick={() => deleteUser(user.id)}>Remover Usuário</button>
+                    </div>
+                ))}
 
-                <div className={style.login}>
-                    <h1 className={style.title}>Bem-vindo ao Recicle365!</h1>
-                    <p className={style.p}>
-                        Faça login para ver os Locais de Coleta de Resíduos disponíveis perto de você.
-                    </p>
-
-                    <form className={style.form}>
-                        <div className={style.email}>
-                            <label htmlFor="email">Email</label>
-                            <input type="email" id="email" placeholder='digite seu email' />
-                        </div>
-                        <div className={style.password}>
-                            <label htmlFor="password">Senha</label>
-                            <input type="password" id="password" placeholder='digite sua senha' />
-                        </div>
-                        <div className={style.button}>
-                            <a className={style.btnLogin} href="./dashboard">Login</a> {/* possívelmente devo usar uma validação que busque o user no json de users cadastrados */}
-                        </div>
-
-                        <div className={style.register}>
-                            <p>Não tem uma conta?</p>
-                            <a className={style.btnRegister} href="./CadastroUser">Cadastre-se</a>
-                        </div>
-                    </form>
-                </div>
             </div>
-        </>
+
+            <div className={style.login}>
+                <h1 className={style.title}>Bem-vindo ao Recicle365!</h1>
+                <p className={style.p}>
+                    Faça login para ver os Locais de Coleta de Resíduos disponíveis perto de você.
+                </p>
+
+                <form className={style.form}>
+                    <div className={style.email}>
+                        <label htmlFor="email">Email</label>
+                        <input type="email" id="email" placeholder='digite seu email' />
+                    </div>
+                    <div className={style.password}>
+                        <label htmlFor="password">Senha</label>
+                        <input type="password" id="password" placeholder='digite sua senha' />
+                    </div>
+                    <div className={style.button}>
+                        <a className={style.btnLogin} href="./dashboard">Login</a> {/* possívelmente devo usar uma validação que busque o user no json de users cadastrados */}
+                    </div>
+
+                    <div className={style.register}>
+                        <p>Não tem uma conta? <a className={style.btnRegister} href="./CadastroUser">Cadastre-se</a> </p>
+                    </div>
+                </form>
+            </div>
+        </div>
     )
 }
 
