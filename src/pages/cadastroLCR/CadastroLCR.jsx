9ee3376ad0557a6f4fee52f393d72bc7dceffb23
Jaqueline Marina <jaquelinemarina@hotmail.com>
2024-04-27 17:22:04 -0300

    feat (cadastroLCR): add campos de input que faltavam

--- src/pages/cadastroLCR/CadastroLCR.jsx
+++ src/pages/cadastroLCR/CadastroLCR.jsx
@@ -58,9 +58,23 @@ function CadastroLCR() {
                     {errors?.description && <p>{errors.description.message}</p>}
                 </div>
 
+
+                <div className={style.cep}>
+                    <label htmlFor="cep">CEP</label>
+                    <input placeholder="digite o CEP"
+                        type="number"
+                        {...register("cep", {
+                            required: "Campo obrigatório.",
+                            minLength: { value: 3, message: "Insira um número válido" },
+                            maxLength: { value: 9, message: "Máximo 9 caracteres" },
+                        })}
+                    />
+                    {errors?.cep && <p>{errors.cep.message}</p>}
+                </div>
+
                 <div className={style.address}> {/* usar dados da API ViaCEP - provavelmente para preenchimento automático */}
-                    <label htmlFor="address">Endereço do local</label>
-                    <input placeholder="digite o endereço"
+                    <label htmlFor="address">Endereço</label>
+                    <input placeholder="digite o seu endereço"
                         type="text"
                         {...register("address", {
                             required: "Campo obrigatório.",
@@ -70,6 +84,83 @@ function CadastroLCR() {
                     />
                     {errors?.address && <p>{errors.address.message}</p>}
                 </div>
+
+                <div className={style.number}>
+                    <label htmlFor="number">Número</label>
+                    <input placeholder="digite o número da sua residência"
+                        type="number"
+                        {...register("number", {
+                            required: "Campo obrigatório.",
+                            minLength: { value: 1, message: "Insira um número válido" },
+                            maxLength: { value: 5, message: "Máximo 5 caracteres" },
+                        })}
+                    />
+                    {errors?.number && <p>{errors.number.message}</p>}
+                </div>
+
+                <div className={style.neighborhood}>
+                    <label htmlFor="neighborhood">Bairro</label>
+                    <input placeholder="digite o seu bairro"
+                        type="text"
+                        {...register("neighborhood", {
+                            required: "Campo obrigatório.",
+                            minLength: { value: 3, message: "Insira um nome válido" },
+                            maxLength: { value: 60, message: "Máximo 60 caracteres" },
+                        })}
+                    />
+                    {errors?.neighborhood && <p>{errors.neighborhood.message}</p>}
+                </div>
+
+                <div className={style.city}>
+                    <label htmlFor="city">Cidade</label>
+                    <input placeholder="digite a cidade"
+                        type="text"
+                        {...register("city", {
+                            required: "Campo obrigatório.",
+                            minLength: { value: 3, message: "Insira um nome válido." },
+                            maxLength: { value: 60, message: "Máximo 60 caracteres." },
+                        })} />
+                    {errors?.city && <p>{errors.city.message}</p>}
+                </div>
+
+                <div className={style.state}>
+                    <label htmlFor="state">Estado</label>
+                    <select name="state" id="state"
+                        {...register("state", {
+                            required: "Por favor, selecione uma opção."
+                        })}>
+                        <option value="">selecione o estado</option>
+                        <option value="AC">AC</option>
+                        <option value="AL">AL</option>
+                        <option value="AP">AP</option>
+                        <option value="AM">AM</option>
+                        <option value="BA">BA</option>
+                        <option value="CE">CE</option>
+                        <option value="DF">DF</option>
+                        <option value="ES">ES</option>
+                        <option value="GO">GO</option>
+                        <option value="MA">MA</option>
+                        <option value="MT">MT</option>
+                        <option value="MS">MS</option>
+                        <option value="MG">MG</option>
+                        <option value="PA">PA</option>
+                        <option value="PB">PB</option>
+                        <option value="PR">PR</option>
+                        <option value="PE">PE</option>
+                        <option value="PI">PI</option>
+                        <option value="RJ">RJ</option>
+                        <option value="RN">RN</option>
+                        <option value="RS">RS</option>
+                        <option value="RO">RO</option>
+                        <option value="RR">RR</option>
+                        <option value="SC">SC</option>
+                        <option value="SP">SP</option>
+                        <option value="SE">SE</option>
+                        <option value="TO">TO</option>
+                    </select>
+                    {errors?.state && <p>{errors.state.message}</p>}
+                </div>
+
 
                 <div className={style.coordinates}>
                     <label htmlFor="coordinates">Cordenadas geográficas</label>
