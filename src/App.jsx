0ddc96b896b04f7672b9ecf11f1961cc291062ee
Jaqueline Marina <jaquelinemarina@hotmail.com>
2024-04-27 01:34:21 -0300

    fix (main): add UsersContextProvider para globalizar função de autenticação

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,15 @@ import './App.css'
 import Header from './components/header/Header.jsx'
 import Footer from './components/footer/Footer.jsx'
 import { Outlet } from 'react-router-dom'
-import { UsersContextProvider } from './context/usersContext'
 
 
 function App() {
 
   return (
     <>
-      <UsersContextProvider>
         <Header />
         <Outlet />
         <Footer />
-      </UsersContextProvider>
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { RouterProvider } from "react-router-dom"
 import routers from "./router/routes"
+import { RouterProvider } from "react-router-dom"
+import { UsersContextProvider } from './context/usersContext'
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <RouterProvider router={routers}>
-    <App />
-  </RouterProvider>
+  <UsersContextProvider>
+    <RouterProvider router={routers}>
+      <App />
+    </RouterProvider>
+  </UsersContextProvider>
 );
