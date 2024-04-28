a459e1088d2a8f37e37cc67818f501257abbcaac
Jaqueline Marina <jaquelinemarina@hotmail.com>
2024-04-28 16:20:50 -0300

    feat (dashboard): criando lista resumida de LCR / parágrafo de boas vindas

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -1,54 +1,53 @@
 import style from './dashboard.module.css'
 import CardLocals from '../../components/cards/cardLocals'
 import CardUsers from '../../components/cards/cardUsers'
+import { useContext } from 'react'
+import { LocalsContext } from '../../context/localsContext'
+import { Link } from 'react-router-dom'
 
 function Dashboard() {
 
+    const { locals } = useContext(LocalsContext)
+
     return (
         <div className={style.container}>
-            <div className={style.viewOne}>
-                <div className={style.locals}>
-                    {/*exibir uma visão geral dos locais de coletas de resíduos cadastrados*/}
-                    <div className={style.localOne}>
-                        <h4>Local 1</h4>
-                        <p>Endereço 1</p>
-                    </div>
-                    <div className={style.localTwo}>
-                        <h4>Local 2</h4>
-                        <p>Endereço 2</p>
-                    </div>
-                    <div className={style.localThree}>
-                        <h4>Local 3</h4>
-                        <p>Endereço 3</p>
-                    </div>
-                    <div className={style.localFour}>
-                        <h4>Local 4</h4>
-                        <p>Endereço 4</p>
-                    </div>
-                </div>
-
-                <div className={style.localButtons}>
-                    <button type="button">Adicionar</button>
-                    <button type="button">Editar</button>
-                    <button type="button">Excluir</button>
-                </div>
+            <h1>Recicle.<br />
+                Reutilize. <br />
+                Reduza.</h1>
+            <div className={style.locals}>
+                <span>Bem-vindo ao Recilce365, site destinado à reciclagem e preservação ambiental, onde cada ação conta para um planeta mais verde!
+                    Sabia que reciclar uma lata de alumínio economiza energia suficiente para manter uma TV ligada por três horas?
+                    E que a reciclagem de uma tonelada de papel salva cerca de 17 árvores e evita o desperdício de 7.000 galões de água?
+                    E que uma tonelada de plástico reciclado economiza energia equivalente a 1.000-2.000 galões de gasolina?
+                    A reciclagem não apenas reduz a quantidade de resíduos enviados para aterros sanitários, mas também economiza recursos preciosos e reduz a emissão de gases de efeito estufa.
+                </span>
+                <h3>Explore o Recicle365 e descubra como você pode fazer a diferença!</h3>
             </div>
 
-            <div className={style.viewTwo}>
-                <div className={style.cards}>
-                    <div className={style.card}>
-                        <CardLocals />
-                    </div>
+            <div className={style.cards}>
+                <div className={style.card}> <CardLocals /> </div>
+                <div className={style.card}>  <CardUsers /></div>
+            </div>
 
-                    <div className={style.card}>
-                        <CardUsers />
-                    </div>
+            <div className={style.list}>
+                <div className={style.locals}>
+                    <h1>Cuide do planeta, encontre o local de coleta mais próximo de você. </h1>
+                    {!!locals && locals.map((local) => (
+                        <div key={local.id}>
+                            <h2>{local.name}</h2>
+                            <p>{local.city} / {local.state}</p>
+                            <h4>Resíduos aceitos: {local.type}</h4>
+                        </div>
+                    ))}
                 </div>
-                <div className={style.map}>
-                    <h2>Mapa dos Locais de Coleta de Resíduos</h2>
-                    {/*exibir um mapa com a localização dos locais de coletas de resíduos cadastrados 
+                <h1>Preserve o meio ambiente.</h1>
+                <h3>Descubra onde descartar seus resíduos de forma responsável <Link to="/ListagemLCR">clicando aqui!</Link></h3>
+            </div>
+
+            <div className={style.map}>
+                <h2>Mapa dos Locais de Coleta de Resíduos</h2>
+                {/*exibir um mapa com a localização dos locais de coletas de resíduos cadastrados 
                     usar React Leaftlet (em último caso, uma lista para exibir os locais)*/}
-                </div>
             </div>
         </div>
     )
