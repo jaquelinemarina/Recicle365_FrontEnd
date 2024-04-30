f8291163eecc10fbae440a8f117ea0c1bf748838
Jaqueline Marina <jaquelinemarina@hotmail.com>
2024-04-30 09:28:37 -0300

    style (header): organizando disposição dos elementos

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -8,9 +8,11 @@ import { Link } from "react-router-dom"
 function Header() {
     return (
         <header className={style.container}>
-            <Link to="/" className={style.dashboard}>Recicle365</Link>
-            <Link to="/ListagemLCR" className={style.listaLCR}>Locais de Coleta</Link>
-            <Link to="/CadastroLCR" className={style.cadastroLCR}>Cadastrar Local de Coleta</Link>
+            <div className={style.menu}>
+                <Link to="/" className={style.dashboard}>Recicle365</Link>
+                <Link to="/ListagemLCR" className={style.listaLCR}>Locais de Coleta</Link>
+                <Link to="/CadastroLCR" className={style.cadastroLCR}>Cadastrar Local de Coleta</Link>
+            </div>
             <Link to="/Login" className={style.exit}>Sair</Link>
         </header>
     )
