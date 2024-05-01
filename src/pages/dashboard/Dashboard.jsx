17a8a15d1aafb3157d12aeb0dc817158630cd493
Jaqueline Marina <jaquelinemarina@hotmail.com>
2024-05-01 20:13:58 -0300

    style (dashboard): estilizando viewOne

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -4,7 +4,9 @@ import CardUsers from '../../components/cards/cardUsers'
 import { useContext } from 'react'
 import { LocalsContext } from '../../context/localsContext'
 import { Link } from 'react-router-dom'
-// import Map from '../../components/map/map'
+import Map from '../../components/map/map'
+import imgRRR from '../../assets/RRR.png'
+import logo from '../../assets/logo.png'
 
 function Dashboard() {
 
@@ -14,17 +16,21 @@ function Dashboard() {
         <div className={style.container}>
             <div className={style.viewOne}>
                 <div className={style.title}>
-                    <h1>Recicle.<br />
-                        Reutilize. <br />
-                        Reduza.</h1>
-                    <h2>Bem-vindo ao  <br />
-                        Recicle365 <br />
-                        Seu destino para reciclagem e preservação ambiental.</h2>
+                    <div className={style.titleText}>
+                        <h2>Bem-vindo ao</h2>
+                        <img className={style.logo}
+                            src={logo}
+                            alt='logo Recicle365'
+                            width={350} />
+                        <h2>Seu destino para reciclagem e preservação ambiental. </h2>
+                    </div>
+                    <div className={style.titleImg}>
+                        <img src={imgRRR} alt='imagem de reciclagem' width={450} />
+                    </div>
                 </div>
-                <h3>Aqui, cada pequena ação contribui para um planeta mais verde e sustentável!</h3>
 
                 <div className={style.subtitle}>
-                    <span>Sabia que reciclar uma lata de alumínio economiza energia suficiente para manter uma TV ligada por três horas?<br />
+                    <span>Você sabia que reciclar uma lata de alumínio economiza energia suficiente para manter uma TV ligada por três horas?<br />
                         E que a reciclagem de uma tonelada de papel salva cerca de 17 árvores e evita o desperdício de 7.000 galões de água?<br />
                         E que uma tonelada de plástico reciclado economiza energia equivalente a 1.000-2.000 galões de gasolina?<br />
                         Ao descartar seus resíduos de forma consciente e apoiar iniciativas de reciclagem, <br />
@@ -60,7 +66,7 @@ function Dashboard() {
 
             <div className={style.map}>
                 <h1>Cuide do planeta, encontre o local de coleta mais próximo de você. </h1>
-                {/* <Map /> */}
+                <Map />
             </div>
         </div >
     )
